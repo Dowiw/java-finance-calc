726cc75ff5b35ae1b90cfae68d6bcd67c56a7f56
Dowiw <monjie12272@gmail.com>
2025-01-28 15:22:43 +0100

    remove ex

--- Main.java
+++ Main.java
@@ -2,21 +2,6 @@ public class Main {
     public static void main(String[] args) {
         FinanceManager manager = new FinanceManager();
         
-        // Example usage
-        Transaction salary = new Transaction(
-            5000.0,
-            "Monthly Salary",
-            "Income",
-            Transaction.TransactionType.INCOME
-        );
-        
-        Transaction rent = new Transaction(
-            1200.0,
-            "Monthly Rent",
-            "Housing",
-            Transaction.TransactionType.EXPENSE
-        );
-        
         manager.addTransaction(salary);
         manager.addTransaction(rent);
         
